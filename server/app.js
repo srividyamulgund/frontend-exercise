3c9ce77938867f37250f9ca5f603fa3960997de6
Srividya Mulgund <srividya.mulgund@gmail.com>
2021-05-21 16:38:25 +0100

    Imported article, posts and comments data

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const router = require('./router');
 const meta = require("./content/meta.json");
 const app = express();
 const port = 3000;
+const article = require("./content/article.json");
+const posts = require("./content/posts.json");
+const comments = require("../db.json");
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.set('meta', meta);
+app.set('article', article);
+app.set('posts', posts);
+app.set('comments', comments);
 
 app.use('*/static', express.static('public'));
 
